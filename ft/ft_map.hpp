3e3630c0c3d57553d14f4610cda365fc595e652a
mertbarut <mebarut@icloud.com>
2022-02-08 01:40:12 +0100

    worked on maps

--- ft/ft_map.hpp
+++ ft/ft_map.hpp
@@ -6,7 +6,7 @@
 /*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 14:44:43 by mbarut            #+#    #+#             */
-/*   Updated: 2022/02/08 01:34:40 by mbarut           ###   ########.fr       */
+/*   Updated: 2022/02/08 01:39:58 by mbarut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,120 @@
 
 namespace ft
 {
+	template <class _Allocator>
+	struct	_map_node
+	{
+
+	public:
+
+		typedef _map_node<_Allocator>							map_node;
+		typedef typename _Allocator::value_type					value_type;
+		typedef typename _Allocator::difference_type			difference_type;
+		typedef typename _Allocator::pointer					pointer;
+		typedef typename _Allocator::reference					reference;
+	//public:
+
+		value_type				_value;
+		map_node*				_parent;
+		map_node*				_left;
+		map_node*				_right;
+		size_t					_level;
+		size_t					_index;
+		bool					_is_red;
+
+		/* ctor 1 */
+		_map_node() : _value( 0 ), _parent(NULL), _left(NULL), _right(NULL), _level(0), _index(0), _is_red(false)
+		{
+			
+		}
+
+		/* ctor 2 */
+		_map_node(map_node* tree, const value_type& value) : _value(value), _parent(tree), _left(tree), _right(tree), _level(0), _index(0), _is_red(false)
+		{
+			
+		}
+
+		/* copy ctor */
+
+		_map_node(const _map_node& node) : _value(node._value), _parent(node._parent), _left(node._left), _right(node._right), _level(node._level), _index(node._index), _is_red(node._is_red)
+		{
+			
+		}
+
+		/* assignment operator overload */
+		_map_node&		operator=(const _map_node& rhs)
+		{
+			if (this == &rhs)
+				return *this;
+			this->_value = rhs._value;
+			this->_parent = rhs._parent;
+			this->_left = rhs._left;
+			this->_right = rhs._right;
+			this->_is_red = rhs._is_red;
+			return *this;
+		}
+		
+		/* dtor */
+		~_map_node()
+		{
+			
+		}
+
+		/* getters */
+		value_type get_value() const { return this->_value; }
+
+		value_type* get_left() const { return this->_left; }
+
+		value_type* get_right() const { return this->_right; }
+		
+		size_t	get_level() const { return this->_level; }
+		
+		size_t	get_index() const { return this->_index; }
+
+		template <typename Node>
+		static Node	get_min(Node node, const map_node* nil)
+		{
+			if (node == nil)
+				return node;
+			for (; node->_left != nil; node = node->_left) {}
+				return node;
+		}
+
+		template <typename Node>
+		static Node	get_max(Node node, const map_node* nil)
+		{
+			if (node == nil)
+				return node;
+			for (; node->_left != nil; node = node->_left) {}
+				return node;
+		}
+
+		template <typename Node>
+		static void	next(Node& node, const map_node* nil)
+		{
+			if (node->right != nil)
+				node = get_min(node->_right, nil);
+			else if (node == nil || nil->_left == node)
+				node = node->_right;
+			else
+				for (; node->_parent != nil && node == node->_parent->_right; node = node->_parent) {}
+				node = node->_parent;
+		}
+
+		template <typename Node>
+		static void	prev(Node& node, const map_node* nil)
+		{
+			if (node->_left != nil)
+				node = get_max(node->_left, nil);
+			else if (node == nil || nil->_right == node)
+				node = node->_left;
+			else
+				for (; node->_parent != nil && node == node->_parent->_left; node = node->_parent) {}
+				node = node->_parent;
+		}
+
+	};
+
 	template <
 		class Key,
 		class T,
